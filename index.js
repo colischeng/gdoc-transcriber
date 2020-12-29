61bc2921ba463b338575f734492d292da64dcde8
colischeng <colischeng@gmail.com>
2020-12-28 21:51:30 -0800

    Implement speech to text

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-console.log('boop')
+const speech = require("@google-cloud/speech");
+const fs = require("fs");
+
+async function main() {
+  const client = new speech.SpeechClient({ keyFilename: './credentials.json' });
+  const filename = "./resources/recording.wav";
+
+  const file = fs.readFileSync(filename);
+  const audioBytes = file.toString("base64");
+
+  const audio = {
+    content: audioBytes,
+  };
+
+  const config = {
+    encoding: "LINEAR16",
+    languageCode: "en-US",
+    sampleRateHertz: 44100,
+    audioChannelCount: 2,
+  };
+
+  const request = {
+    audio: audio,
+    config: config,
+  };
+
+  const [response] = await client.recognize(request);
+  const transcription = response.results
+    .map((result) => result.alternatives[0].transcript)
+    .join("\n");
+  
+  console.log(`Transcription: ${transcription}`);
+}
+
+main()
+.then(console.log("success")) // TODO: export onto Google Doc
+.catch(console.error);
